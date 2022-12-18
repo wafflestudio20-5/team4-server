cbf9d64726cb33b2003f3c9c58ebee17b0d860c9
dongjoocha <dongjoocha@naver.com>
2022-12-18 19:55:48 +0900

    Set up project

---
+++ src/test/kotlin/com/wafflestudio/toyproject/team4/Team4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wafflestudio.toyproject.team4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
