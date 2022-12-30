c427fa6d1f7813b0b1d0effdeec2e9819b038fdd
Boseong Seo <suzy13549@snu.ac.kr>
2022-12-31 03:23:28 +0900

    fix: 새 유저 객체 생성 시 암호화된 비밀번호 정보를 DB에 저장하도록 수정

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/service/AuthService.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/service/AuthService.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.toyproject.team4.core.user.service
 
+import com.wafflestudio.toyproject.team4.config.AuthConfig
 import com.wafflestudio.toyproject.team4.core.user.api.request.RegisterRequest
 import com.wafflestudio.toyproject.team4.core.user.database.UserRepository
 import org.springframework.stereotype.Service
@@ -12,11 +13,13 @@ interface AuthService {
 
 @Service
 class AuthServiceImpl(
+    private val authConfig: AuthConfig,
     private val userRepository: UserRepository
 ): AuthService {
     
     @Transactional
     override fun register(request: RegisterRequest) {
-        userRepository.save(request.toUserEntity(request.password))
+        val encodedPwd = authConfig.passwordEncoder().encode(request.password)
+        userRepository.save(request.toUserEntity(encodedPwd))
     }
 }
