a8ad09db232642b9b7e83f86a61cb6399f874a57
Boseong Seo <suzy13549@snu.ac.kr>
2023-01-02 23:37:56 +0900

    correct: 쉼표 추가

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/service/AuthService.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/service/AuthService.kt
@@ -1,25 +1,25 @@
 package com.wafflestudio.toyproject.team4.core.user.service
 
-import com.wafflestudio.toyproject.team4.config.AuthConfig
 import com.wafflestudio.toyproject.team4.core.user.api.request.RegisterRequest
 import com.wafflestudio.toyproject.team4.core.user.database.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 
 interface AuthService {
-    fun register(registerRequest: RegisterRequest)
+    fun register(request: RegisterRequest)
 }
 
 @Service
 class AuthServiceImpl(
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
     private val userRepository: UserRepository
 ): AuthService {
     
     @Transactional
-    override fun register(registerRequest: RegisterRequest) {
-        val encodedPassword = passwordEncoder.encode(registerRequest.password)
+    override fun register(request: RegisterRequest) {
+        val encodedPassword = passwordEncoder.encode(request.password)
         userRepository.save(request.toUserEntity(encodedPassword))
     }
 }
