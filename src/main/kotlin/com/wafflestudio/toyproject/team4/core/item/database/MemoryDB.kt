9614e1e90a1a3edcec5b9cca965dea651341585b
Boseong Seo <suzy13549@snu.ac.kr>
2023-01-07 16:09:16 +0900

    Refactor MeMoryDB.kt code
    - 크롤링으로 수집한 데이터로 ItemEntity 생성하는 방식 대폭 수정
    - ItemRepository에 저장까지 확인 완료
    -> 분류 코드에 따라 category, subCategory가 자동으로 그에 부합하는 enum value 들어가도록 수정 필요

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/item/database/MemoryDB.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/item/database/MemoryDB.kt
@@ -1,14 +1,17 @@
 package com.wafflestudio.toyproject.team4.core.item.database
 
+import com.wafflestudio.toyproject.team4.core.item.domain.Item
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Node
 import org.springframework.boot.context.event.ApplicationStartedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
-class MemoryDB {
+class MemoryDB (
+    private val itemRepository: ItemRepository
+) {
     /**
      * 서버가 시작하면, 크롤링을 통해 무신사에서 실시간 랭킹 긁어와서 아이템 repository에 저장
      * 각 대분류 - 소분류별로 5개씩
@@ -26,41 +29,37 @@ class MemoryDB {
          * SHOES : 005       - GOODOO(005014), SANDAL(005004), SLIPPER(005018) // SNEAKERS(mainCategory = 18)
          * HEADWEAR : 007    - CAP(007001), HAT(007004), BEANIE(007005)
          **/
-        
+
         crawling("001", "001006")
     }
-    
-    
-    private fun crawling(mainCategoryId: String, subCategoryId: String){
+
+
+    @Transactional
+    fun crawling(mainCategoryId: String, subCategoryId: String) {
         val url: String = "https://www.musinsa.com/ranking/best?period=now" +
-                           "&mainCategory=$mainCategoryId&subCategory=$subCategoryId"
+            "&mainCategory=$mainCategoryId&subCategory=$subCategoryId"
         val document: Document = Jsoup.connect(url).get()
-        val itemInfoList: List<List<Node>> = document
-            .select("div[class=list-box box]")
-            .select("ul li[class=li_box]")
-            .subList(0, 5) // 각 대분류-소분류별로 5개씩 값 긁어오기
-            .map {
-                it.childNodes().filterIndexed { idx,_ -> idx%2!=0 }
+
+        document.run {
+            val labelInfoList = this.getElementsByClass("box-icon-right").map { it.text() }
+            val imageInfoList = this.getElementsByClass("lazyload lazy").map { it.attr("data-original") }
+            val brandInfoList = this.select("div p[class=item_title] a").map { it.text() }
+            val itemNameList = this.select("div p[class=list_info] a").map { it.attr("title") }
+            val priceInfoList = this.select("div p[class=price]").map { it.text() }
+
+            for (idx in 0..4) {
+                val newItem = ItemEntity(
+                    name = itemNameList[idx],
+                    brand = brandInfoList[idx],
+                    imageUrl = imageInfoList[idx],
+                    //label = labelInfoList[idx],
+                    oldPrice = priceInfoList[idx].substringBefore(" ").dropLast(1).replace(",", "").toLong(),
+                    newPrice = priceInfoList[idx].substringAfter(" ").dropLast(1).replace(",", "").toLong(),
+                    category = Item.Category.TOP,
+                    subCategory = Item.SubCategory.SWEATER
+                )
+                itemRepository.save(newItem)
             }
-
-        return itemInfoList.forEach { itemInfo -> parseItemInfo(itemInfo) }
+        }
     }
-    
-    private fun parseItemInfo(itemInfo: List<Node>) {
-        // 라벨
-        val label = itemInfo[1].childNode(1).childNode(0)
-        // 브랜드
-        
-        // 품목명
-        // 브랜드
-        // 이미지 url
-        // 라벨
-        // 기존가
-        // 신규가
-        // 할인율
-        // 성별
-        // 별점
-        
-    }
-
 }
