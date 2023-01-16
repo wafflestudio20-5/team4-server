a3f3027c36fc21d61c206085c214eacb6dfb1330
Chanhyuk Yang <yangchanhk98@snu.ac.kr>
2023-01-17 06:52:13 +0900

    Refactor: UserEntity

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/UserEntity.kt
@@ -1,12 +1,11 @@
 package com.wafflestudio.toyproject.team4.core.user.database
 
-import com.wafflestudio.toyproject.team4.core.item.database.ItemEntity
 import com.wafflestudio.toyproject.team4.core.user.domain.User
+import com.wafflestudio.toyproject.team4.oauth.entity.ProviderType
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import javax.persistence.*
-
 
 @Entity
 @Table(name = "users")
@@ -18,16 +17,24 @@ class UserEntity(
 
     @Column(unique = true)
     var nickname: String,
-    var imageUrl: String? = null,
+    var image: String? = null,
     var reviewCount: Long? = 0L,
 
     @Enumerated(EnumType.STRING)
     val sex: User.Sex? = null,
     var height: Long? = null,
     var weight: Long? = null,
-    var socialKey: String? = null,
+
+    @Enumerated(EnumType.STRING)
+    var socialKey: ProviderType? = null,
+
+    @Enumerated(EnumType.STRING)
+    val role: User.Role = User.Role.ROLE_USER,
+
     var refreshToken: String? = null,
 ) {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
