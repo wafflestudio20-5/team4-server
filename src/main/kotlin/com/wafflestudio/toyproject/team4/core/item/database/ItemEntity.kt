991e3726aa8b90caa818f9e1459e7a69f622b84a
Boseong Seo <suzy13549@snu.ac.kr>
2023-01-17 17:02:09 +0900

    Add: `reviewCount` field to ItemEntity, Item class

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/item/database/ItemEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/item/database/ItemEntity.kt
@@ -13,7 +13,8 @@ class ItemEntity(
     val label: Item.Label? = null,
     @Enumerated(EnumType.STRING)
     val sex: Item.Sex,
-    val rating: Double? = 0.0,
+    var reviewCount: Long? = 0,
+    var rating: Double? = 0.0,
 
     val oldPrice: Long,
     var newPrice: Long? = null,
