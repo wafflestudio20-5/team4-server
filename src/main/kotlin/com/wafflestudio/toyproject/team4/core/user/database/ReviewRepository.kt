717e0fb0a78087834b35f2523f6c2163fbcf2b7d
Boseong Seo <suzy13549@snu.ac.kr>
2023-01-18 00:49:48 +0900

    Implement fun findAllByItemId in ReviewRepository using queryDSL

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/ReviewRepository.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/ReviewRepository.kt
@@ -1,6 +1,8 @@
 package com.wafflestudio.toyproject.team4.core.user.database
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.toyproject.team4.core.user.database.QPurchaseEntity.purchaseEntity
+import com.wafflestudio.toyproject.team4.core.user.database.QReviewEntity.reviewEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 
@@ -17,6 +19,14 @@ class ReviewRepositoryCustomImpl(
     private val queryFactory: JPAQueryFactory,
 ) : ReviewRepositoryCustom {
     override fun findAllByItemId(itemId: Long): List<ReviewEntity> {
-        return listOf()
+        return queryFactory
+            .select(reviewEntity)
+            .from(reviewEntity)
+            .leftJoin(purchaseEntity)
+            .on(purchaseEntity.review.eq(reviewEntity))
+            .fetchJoin()
+            .where(purchaseEntity.item.id.eq(itemId))
+            .orderBy(reviewEntity.rating.desc())
+            .fetch()
     }
 }
