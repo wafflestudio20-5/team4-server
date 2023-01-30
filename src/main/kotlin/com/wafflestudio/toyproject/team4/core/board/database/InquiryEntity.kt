6ce4d9f093a384a69728a262808b1a5a88863503
Boseong Seo <suzy13549@snu.ac.kr>
2023-01-31 01:00:08 +0900

    Fix: change the enumerated type of the `type` field in InquiryEntity

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/board/database/InquiryEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/board/database/InquiryEntity.kt
@@ -8,14 +8,7 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.Entity
-import javax.persistence.EntityListeners
-import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.ManyToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "inquiries")
@@ -28,6 +21,7 @@ class InquiryEntity(
 
     var title: String,
     var content: String,
+    @Enumerated(EnumType.STRING)
     var type: Type,
     var optionName: String? = null,
 
