1bcc3f174659d033a2dbe1a7ab64c422b551da7b
Chanhyuk Yang <yangchanhk98@snu.ac.kr>
2023-02-01 00:32:18 +0900

    Fix: GET findByUsername에서 purchases를 fetchjoin 하도록 수정

--- src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/UserRepository.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/team4/core/user/database/UserRepository.kt
@@ -1,18 +1,29 @@
 package com.wafflestudio.toyproject.team4.core.user.database
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.toyproject.team4.core.user.database.QUserEntity.userEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 
 interface UserRepository : JpaRepository<UserEntity, Long>, UserRepositoryCustom {
     fun findByUsername(username: String): UserEntity?
-
     fun findByNickname(nickname: String): UserEntity?
 }
 
-interface UserRepositoryCustom
+interface UserRepositoryCustom {
+    fun findByUsernameFetchJoinPurchases(username: String): UserEntity?
+
+}
 
 @Component
 class UserRepositoryCustomImpl(
     private val queryFactory: JPAQueryFactory
-) : UserRepositoryCustom
+) : UserRepositoryCustom {
+    override fun findByUsernameFetchJoinPurchases(username: String): UserEntity? {
+        return queryFactory
+            .selectDistinct(userEntity)
+            .from(userEntity)
+            .leftJoin(userEntity.purchases).fetchJoin()
+            .fetchOne()
+    }
+}
